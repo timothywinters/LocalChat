ae8df1e30728111d27deb74a08559e90790cad52
Timothy Winters <timothywinters@PCSs-MacBook-Pro-2.local>
2014-11-20 18:17:22 -0800

    Initial Commit

---
+++ LocalChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalChat
+//
+//  Created by Timothy Winters on 11/20/14.
+//  Copyright (c) 2014 Timothy Winters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalChat
+//
+//  Created by Timothy Winters on 11/20/14.
+//  Copyright (c) 2014 Timothy Winters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
